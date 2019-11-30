19bd90f32b7ef77dc9aee6ece73b22d89d052875
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-11-30 14:50:18 +0200

    show route

--- routes/todos.js
+++ routes/todos.js
@@ -11,13 +11,24 @@ router.get('/', function(req,res){
         .catch(function(err){
             res.send(err);
         })
-})
+});
 
 router.post('/', function(req,res){
     //creates a new todo
     db.Todo.create(req.body)
         .then(function(newTodo){
             res.status(201).json(newTodo);
+        })
+        .catch(function(err){
+            res.send(err);
+        })
+});
+
+router.get(':/todoId', function(req,res){
+    //getting the id out of the 
+    db.Todo.findById(req.params.todoId)
+        .then(function(foundTodo){
+            res.json(foundTodo);
         })
         .catch(function(err){
             res.send(err);
