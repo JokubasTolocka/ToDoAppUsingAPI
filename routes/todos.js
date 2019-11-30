b5756ff383dacb0233c505eeae6525a7bb606164
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-11-30 15:10:20 +0200

    refactored todos routes into helpers folder

--- routes/todos.js
+++ routes/todos.js
@@ -1,59 +1,15 @@
 var express = require('express'),
     router = express.Router(),
-    db = require('../models');
+    db = require('../models'),
+    helpers = require('../helpers/todos');
 
-router.get('/', function(req,res){
-    //finds all todos
-    db.Todo.find()
-        .then(function(todos){
-            res.json(todos);
-        })
-        .catch(function(err){
-            res.send(err);
-        })
-});
+router.route('/')
+    .get(helpers.getTodos)
+    .post(helpers.createTodo)
 
-router.post('/', function(req,res){
-    //creates a new todo
-    db.Todo.create(req.body)
-        .then(function(newTodo){
-            res.status(201).json(newTodo);
-        })
-        .catch(function(err){
-            res.send(err);
-        })
-});
-
-router.get('/:todoId', function(req,res){
-    //getting the id out of the 
-    db.Todo.findById(req.params.todoId)
-        .then(function(foundTodo){
-            res.json(foundTodo);
-        })
-        .catch(function(err){
-            res.send(err);
-        })
-});
-
-router.put('/:todoId', function(req,res){
-    //update route {new: true} makes it so you wont get the old todo, instead you get the updated todo
-    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
-        .then(function(todo){
-            res.json(todo);
-        })
-        .catch(function(err){
-            res.send(err);
-        })
-});
-
-router.delete('/:todoId', function(req,res){
-    db.Todo.remove({_id: req.params.todoId})
-        .then(function(){
-            res.json({message: "We deleted it!"});
-        })
-        .catch(function(err){
-            res.send(err);
-        })
-});
+router.route('/:todoId')
+    .get(helpers.getTodo)
+    .put(helpers.updateTodo)
+    .delete(helpers.deleteTodo)
 
 module.exports = router;
