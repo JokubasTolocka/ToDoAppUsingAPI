0b7fb6d62fa4a36f8b2e57ad668004e2cb4d1ac7
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-11-30 14:56:38 +0200

    update route

--- routes/todos.js
+++ routes/todos.js
@@ -24,11 +24,22 @@ router.post('/', function(req,res){
         })
 });
 
-router.get(':/todoId', function(req,res){
+router.get('/:todoId', function(req,res){
     //getting the id out of the 
     db.Todo.findById(req.params.todoId)
         .then(function(foundTodo){
             res.json(foundTodo);
+        })
+        .catch(function(err){
+            res.send(err);
+        })
+});
+
+router.put('/:todoId', function(req,res){
+    //update route {new: true} makes it so you wont get the old todo, instead you get the updated todo
+    db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+        .then(function(todo){
+            res.json(todo);
         })
         .catch(function(err){
             res.send(err);
