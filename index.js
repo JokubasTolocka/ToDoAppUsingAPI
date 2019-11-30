313a4d42d86bbd39fc34acd3ea601828737bed88
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-11-30 14:41:59 +0200

    create route

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 var express = require('express'),
     app = express();
     port = process.env.PORT || 3000,
-    todoRoutes = require('./routes/todos');
+    todoRoutes = require('./routes/todos'),
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(res,res){
     res.send("Hello from root route"); 
