be37c37ca98433630b31e525bab72a2efaaa00b6
Jokūbas Toločka <jokubas.tj@gmail.com>
2019-12-01 15:19:36 +0200

    form input works

--- public/app.js
+++ public/app.js
@@ -1,16 +1,39 @@
+// import { createTodo } from "../helpers/todos";
+
 $(document).ready(function(){
     $.getJSON('api/todos')
         .then(addTodos)
+    
+    $('#todoInput').keypress(function(event){
+        if(event.which == 13){
+            createTodo();
+        }
+    })
 });
 
 function addTodos(todos){
     //add todos to the page
     todos.forEach(function(todo){
-        var newTodo = $('<li class="todo">' + todo.name + '</li>');
-        if(todo.completed){
-            newTodo.addClass("done");
-        }
-        $('.list').append(newTodo);
+        addTodo(todo); 
     });
 
 }
+
+function createTodo(){
+    //send request to create a new todo
+    var userInput = $("#todoInput").val();
+    $.post('api/todos',{name: userInput})
+        .then(function(newTodo){
+            addTodo(newTodo);
+            $("#todoInput").val("");
+        })
+}
+
+function addTodo(todo){
+    var newTodo = $('<li class="todo">' + todo.name + '</li>');
+    if(todo.completed){
+        newTodo.addClass("done");
+    }
+    $('.list').append(newTodo);
+}
+
